fafe81793a65d6853e33e36e4361e15e6b974596
Cleiton Correa <cleitoncorrea2013@hotmail.com>
2021-10-15 14:31:02 -0300

    Alteração na conexão local

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 var port = 3000;
 var connection = "localhost";
 const DATA = new Array();
 console.log('teste de conexao...');
-=======
-const PORT = 3000;
-const connection = 'localhost';
-console.log('teste de conexao...' + PORT);
->>>>>>> 9347e3242118b2692c3d3beaabed904059c1f152
