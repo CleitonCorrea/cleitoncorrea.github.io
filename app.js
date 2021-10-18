1b80eac9cb59d5cf3ecb3d8ae53a242f7cecfb15
Cleiton Correa <cleitoncorrea2013@hotmail.com>
2021-10-18 10:24:04 -0300

    Adicionando Upstream

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ var port = 3000;
 var connection = "localhost";
 const DATA = new Array();
 const CONNECTIONSTRING = new Array();
+const UPSTREAM = new Array();
 console.log('teste de conexao...' + port);
